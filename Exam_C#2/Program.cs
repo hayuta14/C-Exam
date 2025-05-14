862085c7ca10919d4b50483296ed6128d7cfac05
hayuta14 <maiducanh1412@gmail.com>
2025-05-14 13:19:14 +0700

    update

--- Exam_C#2/Program.cs
+++ Exam_C#2/Program.cs
@@ -4,24 +4,20 @@ using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// ✅ Sử dụng MySQL
 builder.Services.AddDbContext<ApplicationContextDb>(options =>
 {
     options.UseMySql(
         builder.Configuration.GetConnectionString("connectionDatabaseString"),
         new MySqlServerVersion(new Version(8, 0, 33))
-    // Ghi đúng version của MySQL bạn đang dùng
     );
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
